57dfee6bde7141d2a519b38be001f4ce3abae790
Charley Pinckney <pinckc7@gmail.com>
2016-11-05 23:14:49 -0500

    fixed ads.ctrl.js and ads.proc.js

--- server/procedures/ads.proc.js
+++ server/procedures/ads.proc.js
@@ -4,14 +4,15 @@ var db = require('../config/db');
 exports.procGetAds = function() {
     return db.fnRows('procGetAds');
 }
-exports.procInsertAds = function(adName, imageurl, adLink, publish){
-    return db.fnRow('procInsertUser', [adName, imageurl, adLink, publish])
+//write one for getAd
+exports.procInsertAd = function(adName, imageurl, adLink, publish){
+    return db.fnRow('procInsertAd', [adName, imageurl, adLink, publish])
 }
 
-exports.procUpdateAds = function(adName, imageurl, adLink, publish){
-    return db.fnEmpty('procUpdateUser', [adName, imageurl, adLink, publish]);
+exports.procUpdateAd = function(adName, imageurl, adLink, publish){
+    return db.fnEmpty('procUpdateAd', [adName, imageurl, adLink, publish]);
 }
 
-exports.procDeleteAds = function(id){
-   return db.fnEmpty('procDeleteUser', [id]);
+exports.procDeleteAd = function(id){
+   return db.fnEmpty('procDeleteAd', [id]);
 }
