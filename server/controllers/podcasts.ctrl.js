16f47f9f96463b058ee3c4b779ef9af6b0beb2e6
Shantanu Sahai <5han7anu@gmail.com>
2016-11-30 21:45:23 -0600

    pulling 8 podcasts on home page

--- server/controllers/podcasts.ctrl.js
+++ server/controllers/podcasts.ctrl.js
@@ -5,7 +5,7 @@ var mixcloud = require('mixcloud');
 var router = express.Router();
 
 router.get('/', function(req, res){
-    mixcloud.cloudcasts('substrateradio',{ limit: 6 })
+    mixcloud.cloudcasts('substrateradio',{ limit: 8 })
     .then(function(casts) {
         res.send(casts.results);
     }, function(err) {
