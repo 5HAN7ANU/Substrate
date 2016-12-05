91c316e93bc7154921dad1f94cbf6136bc7bf19f
Shantanu Sahai <5han7anu@gmail.com>
2016-12-04 20:47:59 -0600

    removed dot from ./client_secret.json

--- server/services/calendar.svc.js
+++ server/services/calendar.svc.js
@@ -14,7 +14,7 @@ var TOKEN_PATH = TOKEN_DIR + 'calendar-nodejs-quickstart.json';
 exports.getEventList = function(eventCount, timeMin, timeMax) {
   return new Promise(function(resolve, reject) {
     // Load client secrets from a local file.
-    fs.readFile('./client_secret.json', function processClientSecrets(err, content) {
+    fs.readFile('/client_secret.json', function processClientSecrets(err, content) {
       if (err) {
         console.log('Error loading client secret file: ' + err);
         reject(err);
